b7c8c6c0e7ce83084047ec58ee69fbd0694709bd
Felipperrosa <117481386+Felipperrosa@users.noreply.github.com>
2024-05-17 12:38:32 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import boardgame.Position;
+
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("New chess game");
-        System.out.println("New chess game");
+
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+        
+
 
         sc.close();
     }
