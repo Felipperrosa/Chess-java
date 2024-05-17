f445ba7d4a32425707c79415941c6dd6a0072f0b
Felipperrosa <117481386+Felipperrosa@users.noreply.github.com>
2024-05-17 12:04:56 -0300

    Project created

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("New chess game");
+        System.out.println("New chess game");
 
         sc.close();
     }
