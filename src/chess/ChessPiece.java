b572ecb309fbfce8cb31c6525d2ed7cb8bac283e
Felipperrosa <117481386+Felipperrosa@users.noreply.github.com>
2024-06-05 22:48:14 -0300

    Fixbug(Promotion)

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -23,10 +23,12 @@ public abstract class ChessPiece extends Piece {
         return moveCount;
     }
 
-    public void increaseMoveCount(){
+   protected void increaseMoveCount(){
+
         moveCount++;
     }
-    public void decreaseMoveCount(){
+    protected void decreaseMoveCount(){
+
         moveCount--;
     }
 
